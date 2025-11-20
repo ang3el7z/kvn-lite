4fcd48f33c7cb81388c6855ac0e0a6e6f87ea9e8
Ang3el <ang3el7z@gmail.com>
2025-11-20 14:02:13 +0300

    Initial commit

---
+++ app/service.php
@@ -0,0 +1,27 @@
+<?php
+
+require __DIR__ . '/timezone.php';
+
+require __DIR__ . '/bot.php';
+require __DIR__ . '/config.php';
+require __DIR__ . '/i18n.php';
+
+if ($c['debug']) {
+    require __DIR__ . '/debug.php';
+}
+
+$bot = new Bot($c['key'], $i);
+
+$bot->selfUpdate();
+$bot->ssPswdCheck();
+$bot->restartTG();
+if (!empty($bot->selfupdate)) {
+    $bot->offWarp();
+}
+$bot->dontshowcron = 1;
+$bot->sslip();
+$bot->adguardSync();
+$bot->cloakNginx();
+$bot->syncDeny();
+$bot->cleanDocker();
+$bot->iodineRestart();
---
+++ readme.md
@@ -0,0 +1,24 @@
+telegram bot to manage servers (inside the bot)
+
+- VLESS (Reality OR Websocket)
+- AdguardHome
+- WARP
+- automatic ssl
+
+---
+environment: ubuntu 22.04/24.04, debian 11/12
+
+## Install:
+
+```shell
+wget -O- https://raw.githubusercontent.com/mercurykd/vpnbot/master/scripts/init.sh | sh -s YOUR_TELEGRAM_BOT_KEY master
+```
+#### Restart:
+```shell
+make r
+```
+#### autoload:
+```shell
+crontab -e
+```
+add `@reboot cd /root/vpnbot && make r` and save
